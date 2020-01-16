b0b01628a8130f0771629bdf440a9ed295117704
Sabya7 <sabya62.ss@gmail.com>
2020-01-16 15:58:21 +0530

    initial push

---
+++ src/main/java/com/SabyaJenk/demoJen/DemoJenApplication.java
@@ -0,0 +1,13 @@
+package com.SabyaJenk.demoJen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenApplication.class, args);
+	}
+
+}
